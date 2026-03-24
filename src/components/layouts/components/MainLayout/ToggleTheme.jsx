deeef5f0d88ce16f26f3e324b9e7e7665a7eed2e
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-23 22:47:33 -0600

    Dark toggle y fixed

--- src/components/layouts/components/MainLayout/ToggleTheme.jsx
+++ src/components/layouts/components/MainLayout/ToggleTheme.jsx
@@ -6,7 +6,7 @@ import PlayButton from "@/assets/icons/video/playButton";
 export default function ToggleTheme() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className="w-[clamp(65px,7.109375vw,91px)] flex absolute bottom-10 self-center justify-center gap-[clamp(5px,0.546875vw,7px)] bg-cafe rounded-full">
+    <div className="fixed z-10 w-[clamp(65px,7.109375vw,91px)] flex bottom-10 self-center justify-center gap-[clamp(5px,0.546875vw,7px)] bg-cafe dark:bg-nude rounded-full">
       <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
         <button
           onClick={toggleTheme}
@@ -21,7 +21,7 @@ export default function ToggleTheme() {
       </div>
       <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
         <button className="size-[clamp(18px,2.007813vw,25.7px)] flex justify-center items-center hover:cursor-pointer">
-          <PlayButton className="size-[clamp(15px,1.630469vw,20.87px)]" />
+          <PlayButton className="size-[clamp(15px,1.630469vw,20.87px)] text-nude dark:text-gris" />
         </button>
       </div>
     </div>
