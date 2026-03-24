56baed3cddd4fba84ff5e1e13229088fb38512f5
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-23 22:47:16 -0600

    Overlay en layout

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -5,10 +5,13 @@ import { Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full h-dvh">
       <Navbar />
 
       <ToggleTheme />
+
+      {/* Overlay */}
+      <div className="fixed w-full h-dvh -z-10 bg-linear-to-b from-nude/85 via-nude/75 via-30% to-nude/55 dark:from-gris/95 dark:via-gris/90 dark:via-30% dark:to-gris/65" />
 
       <Outlet />
     </div>
