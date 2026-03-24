635552ef59de2c1023e06c682d27c347bc434ddc
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-24 13:00:57 -0600

    Toggle con alto clamp

--- src/components/layouts/components/MainLayout/ToggleTheme.jsx
+++ src/components/layouts/components/MainLayout/ToggleTheme.jsx
@@ -6,23 +6,25 @@ import PlayButton from "@/assets/icons/video/PlayButton";
 export default function ToggleTheme() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className="fixed z-10 w-[clamp(65px,7.109375vw,91px)] flex bottom-6 self-center justify-center gap-[clamp(5px,0.546875vw,7px)] bg-cafe dark:bg-nude rounded-full">
-      <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
-        <button
-          onClick={toggleTheme}
-          className="size-[clamp(18px,2.007813vw,25.7px)] flex justify-center items-center hover:cursor-pointer"
-        >
-          <img
-            src={theme === "light" ? sunIcon : moonIcon}
-            alt={theme === "light" ? "Sun Icon" : "Moon Icon"}
-            className="size-[clamp(15px,1.630469vw,20.87px)]"
-          />
-        </button>
-      </div>
-      <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
-        <button className="size-[clamp(18px,2.007813vw,25.7px)] flex justify-center items-center hover:cursor-pointer">
-          <PlayButton className="size-[clamp(15px,1.630469vw,20.87px)] text-nude dark:text-gris" />
-        </button>
+    <div className="fixed bottom-0 z-10 flex self-center w-fit h-[clamp(31px,5.46875vw,70px)] justify-center items-center">
+      <div className="w-[clamp(65px,7.109375vw,91px)] h-fit flex justify-center gap-[clamp(5px,0.546875vw,7px)] bg-cafe dark:bg-nude rounded-full">
+        <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
+          <button
+            onClick={toggleTheme}
+            className="size-[clamp(18px,2.007813vw,25.7px)] flex justify-center items-center hover:cursor-pointer"
+          >
+            <img
+              src={theme === "light" ? sunIcon : moonIcon}
+              alt={theme === "light" ? "Sun Icon" : "Moon Icon"}
+              className="size-[clamp(15px,1.630469vw,20.87px)]"
+            />
+          </button>
+        </div>
+        <div className="size-[clamp(30px,3.28125vw,42px)] flex justify-center items-center">
+          <button className="size-[clamp(18px,2.007813vw,25.7px)] flex justify-center items-center hover:cursor-pointer">
+            <PlayButton className="size-[clamp(15px,1.630469vw,20.87px)] text-nude dark:text-gris" />
+          </button>
+        </div>
       </div>
     </div>
   );
