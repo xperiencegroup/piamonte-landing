2a7dcd652915a527b537be922c4019c545484830
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-24 09:34:56 -0600

    Primer ejemplo del icono del cerro de la silla en navbar

--- src/components/layouts/components/MainLayout/Navbar.jsx
+++ src/components/layouts/components/MainLayout/Navbar.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router";
 import LogoPiamonte from "@/assets/logos/main/LogoPiamonte";
+import { useLocation } from "react-router";
+import CerroSilla from "@/assets/icons/shared/cerroSilla";
 
 export default function Navbar() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname.startsWith(path);
+  };
+
   return (
     <nav className="fixed z-10 flex self-center justify-between items-center w-[clamp(300px,95vw,1216px)] h-[clamp(48px,5.234vw,67px)] px-[clamp(14px,1.5625vw,20px)] py-[clamp(5px,0.546875vw,7px)">
       <Link
         to={"/masterplan"}
-        className="w-[clamp(160px,17.5vw,224px)] h-[clamp(38px,4.140625vw,53px)] flex justify-center items-center text-boton text-center font-dmsans uppercase text-gris dark:text-nude"
+        className={`relative flex flex-col w-[clamp(160px,17.5vw,224px)] h-[clamp(38px,4.140625vw,53px)] justify-center items-center text-boton text-center font-dmsans uppercase text-gris dark:text-nude ${isActive("/masterplan") ? "text-cafe" : ""}`}
       >
         Piamonte Xperience
+        <CerroSilla
+          className={`absolute bottom-0 translate-y-[50%] transition-opacity ease ${isActive("/masterplan") ? " text-cafe" : "opacity-0"}`}
+        />
       </Link>
       <Link
         to={"/ubicacion"}
