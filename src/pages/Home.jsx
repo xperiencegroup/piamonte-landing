2263b3ced6b895f899cf66fca4582a5390df4135
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-25 13:28:42 -0600

    Evitar usar fondo en layout, se movió al home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,11 @@ import VerProyectos from "@/sections/Home/VerProyectos";
 export default function Home() {
   return (
     <main className="relative z-0 w-full">
+      {/* Overlay */}
+      <div className="fixed w-full h-dvh -z-10 bg-linear-to-b from-nude/85 via-nude/75 via-30% to-nude/55 dark:from-gris/95 dark:via-gris/90 dark:via-30% dark:to-gris/65" />
+
       <div className="w-full h-dvh" />
+
       {/* Inicio */}
       <Inicio />
 
