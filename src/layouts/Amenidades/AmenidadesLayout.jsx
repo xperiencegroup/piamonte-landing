223655b9612065b132dd52c19fe0a1d388ca092b
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-24 23:03:49 -0600

    Layout para tener pegado outlet arriba del navegador

--- src/layouts/Amenidades/AmenidadesLayout.jsx
+++ src/layouts/Amenidades/AmenidadesLayout.jsx
@@ -32,11 +32,13 @@ export default function AmenidadesLayout() {
 
   const isActive = (path) => location.pathname === path;
   return (
-    <div className="h-dvh w-full">
-      <Outlet />
+    <div className="flex flex-col h-dvh w-full">
+      <div className="flex flex-col grow">
+        <Outlet />
+      </div>
 
       {/* Amenidades Navigation */}
-      <div className="fixed bottom-[clamp(31px,5.46875vw,70px)] w-full">
+      <div className="shrink-0 pb-[clamp(31px,5.46875vw,70px)] w-full">
         <div className="flex justify-around items-end w-full">
           {AMENIDADES_MENU.map((item) => (
             <Link
@@ -44,7 +46,7 @@ export default function AmenidadesLayout() {
               to={item.to}
               className={`relative flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] ${
                 isActive(item.to)
-                  ? "after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-cafe"
+                  ? "after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-cafe dark:after:bg-amarillo"
                   : ""
               }`}
             >
@@ -53,7 +55,7 @@ export default function AmenidadesLayout() {
               )}
 
               <span
-                className={`text-boton-normal uppercase ${isActive(item.to) ? "text-cafe" : "text-gris dark:text-nude"}`}
+                className={`text-boton-normal uppercase ${isActive(item.to) ? "text-cafe dark:text-amarillo" : "text-gris dark:text-nude"}`}
               >
                 {item.label}
               </span>
