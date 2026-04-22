6cea5a5d5fe9616bc6a745696bae845920a1fc3b
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-04-22 16:33:24 -0600

    Navbar con fondo de color

--- src/components/layouts/components/MainLayout/Navbar.jsx
+++ src/components/layouts/components/MainLayout/Navbar.jsx
@@ -51,53 +51,55 @@ export default function Navbar({ navRef }) {
   return (
     <nav
       ref={navRef}
-      className="fixed z-10 flex self-center justify-between items-center w-[clamp(300px,95vw,1216px)] h-[clamp(38px,5.234vw,67px)] px-[clamp(14px,1.5625vw,20px)] py-[clamp(5px,0.546875vw,7px)] max-md:py-0"
+      className="fixed z-10 w-full flex justify-center bg-verde dark:bg-nude"
     >
-      {LEFT_MENU.map((item, index) => {
-        return (
-          <motion.div key={index} style={{ pointerEvents: navbarPointer }}>
-            <Link
-              to={item.to}
-              className={`relative flex flex-col w-[clamp(99px,17.5vw,224px)] h-[clamp(24px,4.140625vw,53px)] justify-center items-center text-boton text-center font-dmsans uppercase hover:font-semibold transition-all ${isActive(item.to) ? "text-cafe dark:text-amarillo font-semibold" : "text-gris dark:text-nude"}`}
-            >
-              <span className="relative">
-                {item.label}
-                <CerroSilla
-                  className={`absolute translate-y-1/8 w-full h-fit transition-opacity ease ${isActive(item.to) ? " text-cafe dark:text-amarillo" : "opacity-0"}`}
-                />
-              </span>
-            </Link>
-          </motion.div>
-        );
-      })}
+      <div className="flex justify-between items-center w-[clamp(300px,95vw,1216px)] h-[clamp(38px,5.234vw,67px)] px-[clamp(14px,1.5625vw,20px)] py-[clamp(5px,0.546875vw,7px)] max-md:py-0">
+        {LEFT_MENU.map((item, index) => {
+          return (
+            <motion.div key={index} style={{ pointerEvents: navbarPointer }}>
+              <Link
+                to={item.to}
+                className={`relative flex flex-col w-[clamp(99px,17.5vw,224px)] h-[clamp(24px,4.140625vw,53px)] justify-center items-center text-boton text-center font-dmsans uppercase hover:font-semibold transition-all ${isActive(item.to) ? "text-nude dark:text-gris font-semibold text-boton-active" : "text-nude dark:text-gris"}`}
+              >
+                <span className="relative">
+                  {item.label}
+                  <CerroSilla
+                    className={`absolute -translate-y-1/8 w-full -left-1/2 translate-x-1/2 transition-opacity ease ${isActive(item.to) ? "text-nude dark:text-gris" : "opacity-0"}`}
+                  />
+                </span>
+              </Link>
+            </motion.div>
+          );
+        })}
 
-      {/* Logo */}
-      <motion.div
-        style={{ opacity: navbarLogoOpacity, pointerEvents: navbarPointer }}
-      >
-        <Link to={"/"} className="justify-center hover:cursor-pointer">
-          <LogoPiamonte className="w-[clamp(87px,15.3125vw,196px)] h-[clamp(16px,2.8125vw,36px)] text-gris dark:text-nude" />
-        </Link>
-      </motion.div>
+        {/* Logo */}
+        <motion.div
+          style={{ opacity: navbarLogoOpacity, pointerEvents: navbarPointer }}
+        >
+          <Link to={"/"} className="justify-center hover:cursor-pointer">
+            <LogoPiamonte className="w-[clamp(87px,15.3125vw,196px)] h-[clamp(16px,2.8125vw,36px)] text-nude dark:text-gris" />
+          </Link>
+        </motion.div>
 
-      {RIGHT_MENU.map((item, index) => {
-        return (
-          <motion.div key={index} style={{ pointerEvents: navbarPointer }}>
-            <Link
-              key={index}
-              to={item.to}
-              className={`relative flex flex-col w-[clamp(99px,17.5vw,224px)] h-[clamp(24px,4.140625vw,53px)] justify-center items-center text-boton text-center font-dmsans uppercase hover:font-semibold transition-all ${isActive(item.to) ? "text-cafe dark:text-amarillo font-semibold" : "text-gris dark:text-nude"}`}
-            >
-              <span className="relative">
-                {item.label}
-                <CerroSilla
-                  className={`absolute translate-y-1/8 w-full h-fit transition-opacity ease ${isActive(item.to) ? " text-cafe dark:text-amarillo" : "opacity-0"}`}
-                />
-              </span>
-            </Link>
-          </motion.div>
-        );
-      })}
+        {RIGHT_MENU.map((item, index) => {
+          return (
+            <motion.div key={index} style={{ pointerEvents: navbarPointer }}>
+              <Link
+                key={index}
+                to={item.to}
+                className={`relative flex flex-col w-[clamp(99px,17.5vw,224px)] h-[clamp(24px,4.140625vw,53px)] justify-center items-center text-boton text-center font-dmsans uppercase hover:font-semibold transition-all ${isActive(item.to) ? "text-nude dark:text-gris font-semibold text-boton-active" : "text-nude dark:text-gris"}`}
+              >
+                <span className="relative">
+                  {item.label}
+                  <CerroSilla
+                    className={`absolute -translate-y-1/4 w-full h-fit transition-opacity ease ${isActive(item.to) ? " text-nude dark:text-gris" : "opacity-0"}`}
+                  />
+                </span>
+              </Link>
+            </motion.div>
+          );
+        })}
+      </div>
     </nav>
   );
 }
