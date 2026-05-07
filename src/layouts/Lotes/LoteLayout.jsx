f0df85aa7f31f1c7182edc2b806559287fd18316
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-05-06 19:28:08 -0600

    Botón de regresar en ambos componentes, icono de click

--- src/layouts/Lotes/LoteLayout.jsx
+++ src/layouts/Lotes/LoteLayout.jsx
@@ -1,8 +1,15 @@
 import { Link, useLocation } from "react-router";
 import { Outlet } from "react-router";
 import HandIcon from "@/assets/icons/home/lotes/handIcon";
+import ReturnIcon from "@/assets/icons/shared/returnIcon";
 
 const LOTE_MENU = [
+  {
+    to: "/masterplan/lotes",
+    type: "return",
+    label: "Regresar",
+    icon: ReturnIcon,
+  },
   {
     to: "vista-360",
     label: "Vista 360°",
@@ -36,7 +43,6 @@ export default function LoteLayout() {
       {/* Navigation */}
       <div className="shrink-0 flex flex-col w-full items-center pb-[clamp(31px,5.46875vw,70px)] px-[clamp(14px,2.5vw,32px)]">
         <>
-          {/* Video Tour */}
           <div className="flex justify-between items-end w-full max-w-[clamp(140px,72.65vw,558px)] lg:max-w-[1216px]">
             {LOTE_MENU.map((item) => {
               const Icon = item.icon;
@@ -46,7 +52,7 @@ export default function LoteLayout() {
                   key={item.to}
                   to={item.to}
                   className={`relative flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] ${
-                    isActive(item.to)
+                    isActive(item.to) && item.type !== "return"
                       ? "after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-cafe dark:after:bg-amarillo"
                       : ""
                   }`}
@@ -60,7 +66,7 @@ export default function LoteLayout() {
 
                   {/* TEXT */}
                   <span
-                    className={`text-boton-normal uppercase whitespace-nowrap ${isActive(item.to) ? "text-cafe dark:text-amarillo font-semibold" : "text-gris dark:text-nude"}`}
+                    className={`text-boton-normal uppercase whitespace-nowrap ${isActive(item.to) && item.type !== "return" ? "text-cafe dark:text-amarillo font-semibold" : "text-gris dark:text-nude"}`}
                   >
                     {item.label}
                   </span>
--- src/sections/Masterplan/MasterplanLotes.jsx
+++ src/sections/Masterplan/MasterplanLotes.jsx
@@ -1,5 +1,7 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import CapaLotesMapped from "@/assets/images/Masterplan/Lotes/CapaLotesMapped";
+import ReturnIcon from "@/assets/icons/shared/returnIcon";
+import ClickIcon from "@/assets/icons/home/clickIcon";
 
 export default function MasterplanLotes() {
   const navigate = useNavigate();
@@ -8,11 +10,30 @@ export default function MasterplanLotes() {
   };
 
   return (
-    <div className="relative z-0 ">
-      {/* Background */}
-      <div className="absolute inset-0 w-full h-dvh">
+    <div className="relative z-0 w-full h-full">
+      {/* Background and button */}
+      <div className="absolute inset-0 w-full h-dvh pointer-events-none">
+        {/* Click Icon */}
+        <div className="absolute z-10 bottom-0 left-1/2 -translate-x-1/2 pb-[clamp(77.03px,11.28125vw,170px)] pointer-events-auto">
+          <ClickIcon className="w-[clamp(12.23px,2.109375vw,27px)] h-fit text-verde dark:text-nude" />
+        </div>
+
+        {/* Return button */}
+        <div className="absolute z-10 bottom-0 left-1/2 -translate-x-1/2 pb-[clamp(31px,5.46875vw,70px)] pointer-events-auto">
+          <Link
+            to={"/masterplan"}
+            className="flex justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] hover:cursor-pointer"
+          >
+            <ReturnIcon className="w-[clamp(9px,1.523438vw,19.5px)] h-[clamp(7px,1.289063vw,16.5px)] text-gris dark:text-nude" />
+
+            <span className="text-boton-normal font-semibold uppercase text-gris dark:text-nude">
+              Regresar
+            </span>
+          </Link>
+        </div>
+
         {/* Overlay */}
-        <div className="linear-gradient-shadow" />
+        <div className="linear-gradient-shadow pointer-events-none" />
       </div>
 
       <div className="fixed inset-0">
