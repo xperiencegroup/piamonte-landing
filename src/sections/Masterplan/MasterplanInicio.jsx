f23ac208800035184a5dfadfc2bcc20b9119a282
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-05-07 12:01:11 -0600

    Bold en texto

--- src/sections/Masterplan/MasterplanInicio.jsx
+++ src/sections/Masterplan/MasterplanInicio.jsx
@@ -20,9 +20,9 @@ export default function MasterplanInicio() {
             to="/masterplan/amenidades"
             aria-disabled={mode === MODE.TRANSITIONING}
             tabIndex={mode === MODE.TRANSITIONING ? -1 : 0}
-            className={`flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] border-b-[0.5px] md:border-b border-gris dark:border-nude ${mode === MODE.TRANSITIONING ? "pointer-events-none" : ""}`}
+            className={`flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] border-b-[0.5px] md:border-b-2 border-gris dark:border-nude ${mode === MODE.TRANSITIONING ? "pointer-events-none" : ""}`}
           >
-            <span className="text-boton-grande text-gris dark:text-nude uppercase">
+            <span className="text-boton-grande text-gris dark:text-nude font-bold uppercase">
               Amenidades
             </span>
           </Link>
@@ -31,9 +31,9 @@ export default function MasterplanInicio() {
             to="/masterplan/lotes"
             aria-disabled={mode === MODE.TRANSITIONING}
             tabIndex={mode === MODE.TRANSITIONING ? -1 : 0}
-            className={`flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] border-b-[0.5px] md:border-b border-gris dark:border-nude ${mode === MODE.TRANSITIONING ? "pointer-events-none" : ""}`}
+            className={`flex w-fit justify-center items-center gap-[clamp(4px,0.78125vw,10px)] p-[clamp(7px,1.25vw,16px)] border-b-[0.5px] md:border-b-2 border-gris dark:border-nude ${mode === MODE.TRANSITIONING ? "pointer-events-none" : ""}`}
           >
-            <span className="text-boton-grande text-gris dark:text-nude uppercase">
+            <span className="text-boton-grande text-gris dark:text-nude font-bold uppercase">
               Lotes
             </span>
           </Link>
