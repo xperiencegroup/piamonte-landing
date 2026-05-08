46f524ca49cb50436e653e9e64a9b08e7e9ad49d
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-05-08 11:35:10 -0600

    Video limpio de youtube

--- src/sections/Nosotros/NosotrosVideo.jsx
+++ src/sections/Nosotros/NosotrosVideo.jsx
@@ -4,7 +4,7 @@ export default function NosotrosVideo() {
       <div className="w-[60%] portrait:w-[95%] lg:w-[clamp(466.41px,80.46875vw,980px)] aspect-video">
         <iframe
           className="w-full h-full"
-          src="https://www.youtube.com/embed/bohAeeNjI6U?autoplay=1&mute=1&controls=1&rel=0&playlist=bohAeeNjI6U"
+          src="https://www.youtube.com/embed/bohAeeNjI6U?autoplay=1&loop=1&mute=1&controls=0&rel=0&modestbranding=1&playlist=bohAeeNjI6U"
           title="Piamonte Residencial Campestre - Xperience AI"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
